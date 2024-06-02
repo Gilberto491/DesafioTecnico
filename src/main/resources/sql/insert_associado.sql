9f9bc5f6b99315d1539a748b2be97e092c2e88e0
Gilberto491 <juniorfredes8@gmail.com>
2024-06-01 23:26:22 -0300

    feat: adicionado insert para associados com cpf's válidos

--- src/main/resources/sql/insert_associado.sql
+++ src/main/resources/sql/insert_associado.sql
@@ -1,11 +1,11 @@
 INSERT INTO Associado (id, cpf, nome, data_criacao, data_atualizacao)
-VALUES (1, '12345678901', 'João Silva', NOW(), NULL),
-       (2, '23456789012', 'Maria Oliveira', NOW(), NULL),
-       (3, '34567890123', 'Carlos Pereira', NOW(), NULL),
-       (4, '45678901234', 'Ana Souza', NOW(), NULL),
-       (5, '56789012345', 'Pedro Santos', NOW(), NULL),
-       (6, '67890123456', 'Fernanda Lima', NOW(), NULL),
-       (7, '78901234567', 'Rafael Almeida', NOW(), NULL),
-       (8, '89012345678', 'Juliana Costa', NOW(), NULL),
-       (9, '90123456789', 'Lucas Fernandes', NOW(), NULL),
-       (10, '01234567890', 'Patrícia Rocha', NOW(), NULL);
+VALUES (1, '92816955028', 'João Silva', NOW(), NULL),
+       (2, '15775203003', 'Maria Oliveira', NOW(), NULL),
+       (3, '32295477050', 'Carlos Pereira', NOW(), NULL),
+       (4, '79009210014', 'Ana Souza', NOW(), NULL),
+       (5, '29046472019', 'Pedro Santos', NOW(), NULL),
+       (6, '99794887005', 'Fernanda Lima', NOW(), NULL),
+       (7, '24062146053', 'Rafael Almeida', NOW(), NULL),
+       (8, '78585362065', 'Juliana Costa', NOW(), NULL),
+       (9, '63496228040', 'Lucas Fernandes', NOW(), NULL),
+       (10, '01234567891', 'Patrícia Rocha', NOW(), NULL);
