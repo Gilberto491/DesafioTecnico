5c0e6777bc26e399b73108ed5660ddcf4a74a198
Gilberto491 <juniorfredes8@gmail.com>
2024-06-02 19:24:27 -0300

    feat: adicionado callback e novo link da api de validação de cpf

--- src/main/java/com/sicredi/desafio/service/impl/CPFValidadorService.java
+++ src/main/java/com/sicredi/desafio/service/impl/CPFValidadorService.java
@@ -2,26 +2,34 @@ package com.sicredi.desafio.service.impl;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-@AllArgsConstructor
 public class CPFValidadorService {
 
-    private static final String API_URL = "https://consultacpf-tbc9.onrender.com/api/validate";
+    private static final String API_URL_PATH = "/api/validate";
+
+    @Value("${callback.url.domain.externa}")
+    private String apiUrlDomain;
+
     private static final Logger logger = LoggerFactory.getLogger(CPFValidadorService.class);
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
 
+    public CPFValidadorService(RestTemplate restTemplate, ObjectMapper objectMapper) {
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+    }
+
     public boolean validaCpf(String cpf) {
 
-        String url = API_URL + "/" + cpf;
+        String url = apiUrlDomain + API_URL_PATH + "/" + cpf;
 
         try {
             ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
